605f6df90fc8fa87ca12bae15877e34345374b05
Rumazor <th3rum2084@gmail.com>
2022-04-19 16:59:05 -0500

    pagination

---
+++ displayFollowers.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container')
+
+const display = (followers) => {
+    const newFollowers = followers.map((user)=>{
+        const {avatar_url:image, html_url:url,login:name} = user
+        return `
+        <article class="card">
+        <img src="${image}" alt="${name}"/>
+        <h4>${name}</h4>
+        <a href="${url}" class="btn">View Profile</a>
+        </article>
+        `
+    }).join('')
+
+    container.innerHTML = newFollowers
+}
+
+
+
+
+export default display
